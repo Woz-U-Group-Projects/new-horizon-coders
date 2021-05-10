a210f653446eb183160c638d4afc891c939f4850
lvlindav <lvlindav23@gmail.com>
2021-05-09 22:13:47 -0500

    added paths to appointments and store/home created simple login

--- client-angular/src/app/app-routing.module.ts
+++ client-angular/src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppointmentsComponent } from './appointments/appointments.component';
+import { StoreComponent } from './store/store.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+   redirectTo: '/home',
+   pathMatch:'full'
+  },
+  {
+    path:'home',
+    component: StoreComponent
+  },
+  {
+   path:'appointments',
+   component: AppointmentsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client-angular/src/app/app.module.ts
+++ client-angular/src/app/app.module.ts
@@ -9,10 +9,11 @@ import { AppointmentsComponent } from './appointments/appointments.component';
 import { AppointmentsService } from "./appointments.service";
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgbdDatepickerBasic } from './datepicker';
-import { NgbdTimepickerBasic } from './timepicker'
+import { NgbdTimepickerBasic } from './timepicker';
+import { StoreComponent } from './store/store.component'
 
 @NgModule({
-  declarations: [AppComponent, TaskDisplayComponent, AppointmentsComponent, NgbdDatepickerBasic, NgbdTimepickerBasic ],
+  declarations: [AppComponent, TaskDisplayComponent, AppointmentsComponent, NgbdDatepickerBasic, NgbdTimepickerBasic, StoreComponent ],
   imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule, NgbModule,],
   providers: [AppointmentsService],
   bootstrap: [AppComponent]
